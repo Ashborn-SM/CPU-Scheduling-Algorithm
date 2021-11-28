1adb57ccf1de0b903363f21008ba8e87f46ca29e
Ashborn-SM <rahulpillai0128@gmail.com>
2021-11-28 13:13:45 +0530

    Fix bug

--- src/macro.h
+++ src/macro.h
@@ -1,10 +1,10 @@
 #ifndef MACROS
 #define MACROS
 
-#define BURST_TIME(process) process.info.burst_t
-#define ARRIVAL_TIME(process) process.info.arrival_t
-#define PRIORITY(process) process.info.priority
-#define ID(process) process.info.id
-#define STATE(process) process.info.state
+#define BURST_TIME(process) process->info.burst_t
+#define ARRIVAL_TIME(process) process->info.arrival_t
+#define PRIORITY(process) process->info.priority
+#define ID(process) process->info.id
+#define STATE(process) process->info.state
 
 #endif
