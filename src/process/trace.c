5d43b10e8bf28afcb031240562e85e3d53974397
Rahul Rajeev Pillai <66192267+Ashborn-SM@users.noreply.github.com>
2021-12-07 19:06:26 +0530

    Change output

--- src/process/trace.c
+++ src/process/trace.c
@@ -1,7 +1,10 @@
 #include "trace.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 void UpdateTrace(char* id, int start_t, int end_t, Trace* trace){
+
     trace->id = id;
     if(start_t != -1){
         trace->start_t = start_t;
@@ -11,13 +14,23 @@ void UpdateTrace(char* id, int start_t, int end_t, Trace* trace){
     }
 }
 
-void render(Trace** trace_array){
-    int idx = 0;
+void render(Trace** trace_array, int ttl_process){
+    int idx = 0, start_t = 0, end_t = 0;
+    char* id;
+    float ttl_time = 0;
+    printf("Trace     \tWaiting time\n\n");
     while(trace_array[idx] != NULL){
-        printf("|____|%i\n", trace_array[idx]->start_t);
-        printf("|%s  | \n", trace_array[idx]->id);
+        id = trace_array[idx]->id;
+        start_t= trace_array[idx]->start_t;
+        end_t = trace_array[idx]->end_t;
+
+        printf("|____|%i\n", start_t);
+        printf("|%s  |\t\t  %s -> %i\n", id, id, start_t);
+        ttl_time += start_t;
         idx++;
     }
-    printf("|____|%i\n", trace_array[idx-1]->end_t);
+    printf("|____|%i\n", end_t);
+
+    printf("\nAverage Waiting Time: %f\n", ttl_time/ttl_process);
 }
 
